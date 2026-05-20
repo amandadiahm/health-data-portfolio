4388f4405567438194670034af5253a57b8d74b1
Amanda Diah Maharani <amanda.dmaharani@gmail.com>
2026-05-20 21:03:39 +1000

    initial website

---
+++ struktur_folder.R
@@ -0,0 +1,7 @@
+#struktur folder
+
+
+dir.create("data/raw", recursive = TRUE)
+dir.create("data/processed", recursive = TRUE)
+dir.create("projects/project1", recursive = TRUE)
+dir.create("images")
