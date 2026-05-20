b81b0c1b77bc8f82c6da4f6d1a1583c7d1d72116
Amanda Diah Maharani <amanda.dmaharani@gmail.com>
2026-05-20 22:48:57 +1000

    add project1 analysis

--- struktur_folder.R
+++ struktur_folder.R
@@ -5,3 +5,16 @@ dir.create("data/raw", recursive = TRUE)
 dir.create("data/processed", recursive = TRUE)
 dir.create("projects/project1", recursive = TRUE)
 dir.create("images")
+
+
+install.packages("quarto")
+library(quarto)
+
+
+
+system('git config user.name "Amanda Diah Maharani"')
+system('git config user.email "amanda.dmaharani@gmail.com"')
+
+
+system("git config user.name")
+system("git config user.email")
